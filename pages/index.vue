4107c6307fa424e097f67fea866f8b15fa1306f6
Zhussipov Meirambek <38666677+bek-app@users.noreply.github.com>
2021-02-12 18:06:43 +0600

    added slider

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,45 @@
         <b-navbar-nav align="right">end</b-navbar-nav>
       </b-collapse>
     </b-navbar>
+    <div class="catalog">
+ 
+      <div class="catalog__content">
+             <div class="left__arrow">
+        <a href="#" class="img">prev</a>
+
+      </div>
+        <div class="catalog__item">
+          <p>Бортики</p>
+          <a href="#">Перейти</a>
+         
+        </div>
+
+        <div class="catalog__item">
+          <p>Балдахины</p>
+          <a href="#">Перейти</a>
+        </div>
+        <div class="catalog__item">
+          <p>Коконы</p>
+          <a href="#">Перейти</a>
+        </div>
+        <div class="catalog__item">
+          <p>Конверт</p>
+          <a href="#">Перейти</a>
+        </div>
+        <div class="catalog__item">
+          <p>Косички</p>
+          <a href="#">Перейти</a>
+        </div>
+        <div class="catalog__item">
+          <p>Постельное белье</p>
+          <a href="#">Перейти</a>
+        </div>
+              <div class="right__arrow">
+        <a href="#"><img src="" alt="">next</a>
+      </div>
+      </div>
+
+    </div>
   </b-container>
 </template>
 
@@ -19,6 +58,9 @@ export default {};
 </script>
 
 <style scoped>
+ 
+@import url("../assets/css/main.css");
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap");
 .container-fluid {
   padding: 0;
   margin: 0;
@@ -27,4 +69,5 @@ export default {};
   display: flex;
   justify-content: space-between;
 }
+
 </style>
