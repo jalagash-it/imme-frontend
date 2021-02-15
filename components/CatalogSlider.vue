3bf64231e545a3e612f9c3dcf85530ab1419877e
Zhussipov Meirambek <38666677+bek-app@users.noreply.github.com>
2021-02-15 14:35:24 +0600

    ozgeris

--- components/CatalogSlider.vue
+++ components/CatalogSlider.vue
@@ -1,44 +1,44 @@
 <template>
-  <b-container fluid> <div class="catalog">
-    <div class="catalog__content">
-      <div class="left__arrow">
-        <a href="#"> <img src="../assets/img/prev.png" alt="" /></a>
-      </div>
-      <div class="catalog__item">
-        <p>Бортики</p>
-        <a href="#">Перейти</a>
-      </div>
+  <b-container fluid>
+    <div class="catalog">
+      <div class="catalog__content">
+        <div class="left__arrow">
+          <a href="#"> <img src="../assets/img/prev.png" alt="" /></a>
+        </div>
+        <div class="catalog__item">
+          <p>Бортики</p>
+          <a href="#">Перейти</a>
+        </div>
 
-      <div class="catalog__item">
-        <p>Балдахины</p>
-        <a href="#">Перейти</a>
-      </div>
-      <div class="catalog__item">
-        <!-- <img src="../assets/img/3.png" alt="" /> -->
-        <p>Коконы</p>
-        <a href="#">Перейти</a>
-      </div>
-      <div class="catalog__item">
-        <!-- <img src="../assets/img/4.png" alt="" /> -->
-        <p>Конверт</p>
-        <a href="#">Перейти</a>
-      </div>
-      <div class="catalog__item">
-        <!-- <img src="../assets/img/5.png" alt="" /> -->
-        <p>Косички</p>
-        <a href="#">Перейти</a>
-      </div>
-      <div class="catalog__item">
-        <!-- <img src="../assets/img/6.png" alt="" /> -->
-        <p>Постельное белье</p>
-        <a href="#">Перейти</a>
-      </div>
-      <div class="right__arrow">
-        <a href="#"><img src="../assets/img/next.png" alt="" /> </a>
-      </div>
-    </div>
-  </div></b-container>
-  
+        <div class="catalog__item">
+          <p>Балдахины</p>
+          <a href="#">Перейти</a>
+        </div>
+        <div class="catalog__item">
+          <!-- <img src="../assets/img/3.png" alt="" /> -->
+          <p>Коконы</p>
+          <a href="#">Перейти</a>
+        </div>
+        <div class="catalog__item">
+          <!-- <img src="../assets/img/4.png" alt="" /> -->
+          <p>Конверт</p>
+          <a href="#">Перейти</a>
+        </div>
+        <div class="catalog__item">
+          <!-- <img src="../assets/img/5.png" alt="" /> -->
+          <p>Косички</p>
+          <a href="#">Перейти</a>
+        </div>
+        <div class="catalog__item">
+          <!-- <img src="../assets/img/6.png" alt="" /> -->
+          <p>Постельное белье</p>
+          <a href="#">Перейти</a>
+        </div>
+        <div class="right__arrow">
+          <a href="#"><img src="../assets/img/next.png" alt="" /> </a>
+        </div>
+      </div></div
+  ></b-container>
 </template>
 
 <style scoped>
@@ -58,21 +58,16 @@
 }
 .left__arrow {
   text-align: center;
- 
 }
 .right__arrow {
   text-align: center;
- 
 }
 .catalog__item {
   position: relative;
   width: 193px;
   height: 319px;
- 
-  border: 4px solid #ffffff;
- 
-  
 
+  border: 4px solid #ffffff;
 
   border-radius: 30px;
   box-shadow: 5px 5px 30px rgba(41, 41, 167, 0.12);
@@ -80,9 +75,8 @@
   flex-direction: column;
   align-items: center;
   justify-content: flex-end;
- 
 }
- 
+
 .catalog__item {
   background: url("../assets/img/2.png") no-repeat center center / cover;
 }
