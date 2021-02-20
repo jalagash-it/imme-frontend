80b87963df39e61f76bd466217edb468e1c68e28
Zhussipov Meirambek <38666677+bek-app@users.noreply.github.com>
2021-02-20 21:02:59 +0600

    ozgertuler

--- pages/succsess.vue
+++ pages/succsess.vue
@@ -1,5 +1,11 @@
  <template>
   <b-container fluid class="succses">
+      <b-row>
+      <b-col>
+        <h1 class="title">Оплата</h1>
+        <b-breadcrumb :items="items"></b-breadcrumb
+      ></b-col>
+    </b-row>
     <b-row>
       <b-col class="succses__content">
         <h class="succses__content-title"> Поздравляем! </h>
@@ -11,19 +17,68 @@
     </b-row>
   </b-container>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          text: "Главная",
+          href: "#",
+        },
+
+        {
+          text: "Корзина и оформление заказа",
+          active: true,
+        },
+      ],
+    };
+  },
+};
+</script>
 <style scoped>
+.breadcrumb {
+  background-color: #f7f9fc;
+  margin-bottom: 48px;
+}
+.breadcrumb-item + .breadcrumb-item::before {
+   content: "";
+ 
+}
+.breadcrumb a {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 13px;
+  line-height: 16px;
+
+  color: #60617b;
+}
+.breadcrumb .active span {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 13px;
+  line-height: 16px;
+
+  color: #9fa1cb;
+}
 .succses {
   background: #f7f9fc;
   width: 100%;
   height: 100vh;
-  display: flex;
-  align-items: center;
+ 
+ 
   justify-content: center;
   font-family: "Montserrat";
 }
+.title{
+  margin-top: 48px;
+  margin-bottom: 16px;
+}
 .succses__content {
+  margin: 0 25px;
   background: url("../assets/img/succsess.png") no-repeat center center / 100%;
  width: 1271px;
+ box-shadow: 5px 5px 30px rgba(41, 41, 167, 0.12);
  
   height: 392px;
   border-radius: 20px;
